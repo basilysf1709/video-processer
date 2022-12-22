66d9863c822a5e807c197655963f45333ab4e239
Basil Yusuf <basilyusuf1709@gmail.com>
2022-12-21 23:35:43 -0500

    Added comments

--- video-processor.py
+++ video-processor.py
@@ -1,6 +1,6 @@
 from moviepy.editor import *
 
-
+# editing clip to remove audio
 clip = VideoFileClip("assets/clip1.mp4")
 edit_clip = clip.without_audio()
 edit_clip.write_videofile("output/final1.mp4")
