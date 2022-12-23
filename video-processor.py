07332a8494f3cea9de1ecc6a71d5872c70233ae1
Basil Yusuf <basilyusuf1709@gmail.com>
2022-12-22 21:34:32 -0500

    Added some functions

--- video-processor.py
+++ video-processor.py
@@ -1,29 +1,47 @@
 from moviepy.editor import *
 
+# function to resize video to same size
+def resize_clip(clip):
+    try:
+        return clip.resize(height=360)
+    except Exception as e:
+        print(e)
+        return
+
 # function to remove audio of a clip
-def removeAudio(clip):
+def remove_audio(clip):
     try:
         edit_clip = clip.without_audio()
         return edit_clip
     except Exception as e:
-        print("Error Occured")
+        print(e)
         return 
 
 # function to combine different clips
-def combineClips(list_of_clips, final_clip_number):
+def combine_clips(list_of_clips, final_clip_number):
     try:
         combined = concatenate_videoclips(list_of_clips)
         combined.write_videofile("output/final{}.mp4".format(final_clip_number))
     except Exception as e:
-        print("Error occured")
+        print(e)
+
+# function to extract audio from a video
+def extract_audio(clip):
+    try:
+        audio = clip.audio
+        return audio
+    except Exception as e:
+        print(e)
+        return
 
 clip1 = VideoFileClip("assets/clip1.mp4")
 clip2 = VideoFileClip("assets/clip2.mp4")
-clip3 = VideoFileClip("assets/clip3.mp4")
+clip3 = VideoFileClip("assets/clip5.mp4")
 clip4 = VideoFileClip("assets/clip4.mp4")
 
 clips = [clip1, clip2, clip3, clip4]
 
 for i in range(len(clips)):
-    clips[i] = removeAudio(clips[i])
-combineClips(clips, 1)
+    clips[i] = resize_clip(clips[i])
+    clips[i] = remove_audio(clips[i])
+combine_clips(clips, 1)
