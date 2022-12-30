4295ffbc7d89217a57ca01b2e8f11bf6820a9ade
Basil Yusuf <basilyusuf1709@gmail.com>
2022-12-29 23:11:24 -0500

    Some changes

--- color-grade.py
+++ color-grade.py
@@ -3,17 +3,14 @@ import numpy as np
 
 # Create a VideoCapture object
 cap = cv2.VideoCapture("assets/clip2.mp4")
-cap.set(3,640)
-cap.set(4,480)
- 
+
 # Check if camera opened successfully
 if (cap.isOpened() == False): 
     print("Unable to read video feed")
 
 # Defining the Codec
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-out = cv2.VideoWriter('output/final.mp4',fourcc, 20.0, (640,480))
- 
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter('output/final.mp4', fourcc, 20.0, (int(cap.get(3)),int(cap.get(4))) )
 while True:
     ret, frame = cap.read()
     out.write(frame)
@@ -23,8 +20,7 @@ while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
-        break 
+        break
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
